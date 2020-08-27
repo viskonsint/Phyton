6ebd84f5e398575c5a00afa39a568933b50f1cf8
viskonsint <70268340+viskonsint@users.noreply.github.com>
2020-08-27 14:59:29 +0600

    Add files via upload

---
+++ Урок2-2.py
@@ -0,0 +1,12 @@
+average_amount = int(input("Какое общее число элементов в списке"))
+List = []
+i = 0
+w = 0
+while i < average_amount:
+    List.append((input("Какое следующее в списке значение"))
+    i += 1
+
+for elem in range(int(len(output)/2)):
+    List[w], List[w + 1] = List [w + 1], List[w]
+        w += 2
+print(output)
